c55ba345ca2b0193e7031295c7ac8929bb1cedd4
Juwon Do <nobeldr13@gmail.com>
2023-01-26 12:29:36 -0600

    exercise

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,17 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      year: "2023"
+      year: "2023",
+      showInfo: true,
+      image: "https://media.gettyimages.com/id/1399037422/photo/justin-thomas-of-the-usa-celebrates-with-the-wanamaker-trophy-after-the-final-round-of-the.jpg?s=612x612&w=gi&k=20&c=PEg1Y_edTrX3OlvPSi7CQWYB0_J6VNCnwgSOra1UH5U=",
+      golfs: ["titleist","callaway","taylormade"]
     };
   },
   created: function() {},
   methods: {
     printSomething: function() {
       console.log("Print out from printSomething method")
+      this.showInfo = !this.showInfo
     }
   }
 };
@@ -20,7 +24,16 @@ export default {
   <div class="home">
     <h1>{{ message }}</h1>
     <h2>{{ year }}</h2>
+    <div v-for=" golf in golfs" :key="golf.id">
+      <p>{{ golf }}</p>
+    </div>
+    <img v-bind:src = "image"/>
+    <p>Year: <input type="text" v-model="year"/></p>
+   
     <button v-on:click="printSomething">Print</button>
+    <div v-if="showInfo">
+      <p>Toggle</p>
+    </div>
   </div>
 </template>
 
