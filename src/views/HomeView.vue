dad78acea247fe027818631a97c4b394dab33116
Juwon Do <nobeldr13@gmail.com>
2023-01-26 13:15:57 -0600

    create OldHomeView to create new one and add index actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,25 @@
 /* eslint-disable */
 <script>
+import axios from 'axios'
+
+
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      year: "2023",
-      showInfo: true,
-      image: "https://media.gettyimages.com/id/1399037422/photo/justin-thomas-of-the-usa-celebrates-with-the-wanamaker-trophy-after-the-final-round-of-the.jpg?s=612x612&w=gi&k=20&c=PEg1Y_edTrX3OlvPSi7CQWYB0_J6VNCnwgSOra1UH5U=",
-      golfs: ["titleist","callaway","taylormade"]
+      products: []
     };
   },
-  created: function() {},
+  created: function() {
+    this.getData();
+  },
   methods: {
-    printSomething: function() {
-      console.log("Print out from printSomething method")
-      this.showInfo = !this.showInfo
+    getData: function() {
+      console.log("Get Data")
+      axios.get("http://localhost:3000/products.json").then(response => {
+        this.products = response.data        
+        console.log(response.data)
+      })
     }
   }
 };
@@ -23,18 +28,14 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h2>{{ year }}</h2>
-    <div v-for=" golf in golfs" :key="golf.id">
-      <p>{{ golf }}</p>
+    <div v-for="product in products" v-bind:key="product.id">
+      <p>{{ product.name}}</p>
+      <p>{{ product.price}}</p>
+      <p>{{ product.description}}</p>
+      <img v-bind:src = "product.image" />
+      <hr />
     </div>
-    <img v-bind:src = "image"/>
-    <p>Year: <input type="text" v-model="year"/></p>
-   
-    <button v-on:click="printSomething">Print</button>
-    <div v-if="showInfo">
-      <p>Toggle</p>
-    </div>
-  </div>
+</div>
 </template>
 
 <style></style>
---
+++ src/views/OldHomeView.vue
@@ -0,0 +1,40 @@
+/* eslint-disable */
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      year: "2023",
+      showInfo: true,
+      image: "https://media.gettyimages.com/id/1399037422/photo/justin-thomas-of-the-usa-celebrates-with-the-wanamaker-trophy-after-the-final-round-of-the.jpg?s=612x612&w=gi&k=20&c=PEg1Y_edTrX3OlvPSi7CQWYB0_J6VNCnwgSOra1UH5U=",
+      golfs: ["titleist","callaway","taylormade"]
+    };
+  },
+  created: function() {},
+  methods: {
+    printSomething: function() {
+      console.log("Print out from printSomething method")
+      this.showInfo = !this.showInfo
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2>{{ year }}</h2>
+    <div v-for=" golf in golfs" :key="golf.id">
+      <p>{{ golf }}</p>
+    </div>
+    <img v-bind:src = "image"/>
+    <p>Year: <input type="text" v-model="year"/></p>
+   
+    <button v-on:click="printSomething">Print</button>
+    <div v-if="showInfo">
+      <p>Toggle</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
