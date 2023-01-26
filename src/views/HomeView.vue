2c94ac045bfb0e0b6bec03ac5a7259f72fba4e05
Juwon Do <nobeldr13@gmail.com>
2023-01-26 15:21:18 -0600

    add create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,14 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      products: []
+      products: [],
+      product: {
+        "name": "",
+        "price": "",
+        "description": "",
+        "quantity": "",
+        "supplier_id": ""
+      }
     };
   },
   created: function() {
@@ -20,6 +27,14 @@ export default {
         this.products = response.data        
         console.log(response.data)
       })
+    },
+    newProduct: function() {
+      console.log("Create new");
+      axios.post("http://localhost:3000/products.json", this.product).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.product = {};
+      })
     }
   }
 };
@@ -28,7 +43,17 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h2>{{ product }}</h2>
+    <p>Name: <input type="text" v-model="product.name" /></p>
+    <p>Price: <input type="text" v-model="product.price" /></p>
+    <p>Description: <input type="text" v-model="product.description" /></p>
+    <p>Quant: <input type="text" v-model="product.quantity" /></p>
+    <p>Supplier_id: <input type="text" v-model="product.supplier_id" /></p>
+    <button v-on:click="newProduct">New Product</button>
+
+
     <div v-for="product in products" v-bind:key="product.id">
+      <h3>ID: {{ product.id}}</h3>
       <p>{{ product.name}}</p>
       <p>{{ product.price}}</p>
       <p>{{ product.description}}</p>
